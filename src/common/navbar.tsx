b185201694fa3da5677309be1f3bd49645daa0cb
yadar <penphatulaya@icloud.com>
2024-05-08 12:51:34 +0700

    no message

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -1,24 +1,23 @@
 import Image from 'next/image';
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 interface NavbarProps {
   profileImage?: string;
 }
 
-const Navbar = ({ profileImage }: NavbarProps)  => {
+ const Navbar = ({ profileImage }: NavbarProps) => {
   return (
-  <nav className="top-0 z-50 bg-white border-gray-200">
-    <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-      <span className="text-sm font-semibold whitespace-nowrap">COSCI Assistant</span>
-      <div className="flex flex-wrap items-center gap-3">
-        <button>
-          <Image src="/musical-bell-outline.png" alt="BELL IMAGE" width={25} height={25} />
-        </button>
-        {profileImage && (
-          <Image src={profileImage} alt="Profile" className="object-cover rounded-full" width={25} height={25} />
-        )}
+    <nav className="top-0 z-50 bg-white border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <span className="self-center text-sm font-semibold whitespace-nowrap">COSCI Assistant</span>
+        <div className="flex flex-wrap items-left gap-3">
+          <button>
+            <Image src="/musical-bell-outline.png" alt="BELL IMAGE" width={25} height={25} />
+          </button>
+          {profileImage && (
+            <Image src={profileImage} alt="Profile" className="object-cover rounded-full" width={25} height={25} />
+          )}
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 };
 
