03aafeb68e21d8b65c1d17c1728c8ff6889346a2
yadar <penphatulaya@icloud.com>
2024-05-08 13:32:52 +0700

    no message

--- src/common/navbar.tsx
+++ src/common/navbar.tsx
@@ -7,7 +7,7 @@ interface NavbarProps {
   return (
     <nav className="top-0 z-50 bg-white border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <span className="self-center text-sm font-semibold whitespace-nowrap">COSCI Assistant</span>
+        <span className="text-sm font-semibold whitespace-nowrap">COSCI Assistant</span>
         <div className="flex flex-wrap items-left gap-3">
           {profileImage && (
             <Image src={profileImage} alt="Profile" className="object-cover rounded-full" width={25} height={25} />
